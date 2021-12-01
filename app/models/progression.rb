394c08d9c68e5159d688ae350abdd47a26cece35
brunamartinii <95253930+brunamartinii@users.noreply.github.com>
2021-12-01 00:49:52 -0300

    Update progression.rb

--- app/models/progression.rb
+++ app/models/progression.rb
@@ -2,4 +2,12 @@ class Progression < ApplicationRecord
   belongs_to :sale
 
   validates :stage, uniqueness: { scope: :sale }
+
+  def days_ago
+    (Date.today - created_at.to_date).to_i
+  end
+
+  def closed?
+    stage == "closed"
+  end
 end
